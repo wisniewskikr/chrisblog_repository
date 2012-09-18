823d0645da3b4f8ab046dd73f44fd2f1f9f2dfe0
Krzysztof Wisniewski <wisniewskik@op.pl>
2012-09-18 12:03:09 +0200

    Change profiles

---
+++ ChrisBlog_New/src/test/java/pl/kwi/chrisblog/utils/DateUtilsIntg.java
@@ -0,0 +1,14 @@
+package pl.kwi.chrisblog.utils;
+
+import org.junit.Test;
+
+public class DateUtilsIntg {
+	
+	@Test
+	public void tmp1(){
+		
+		System.out.println("***** HERE *****");
+		
+	}
+
+}
