945e843292e21466ceed6596943ab63b4a400af7
Krzysztof Wisniewski <wisniewskik@op.pl>
2012-09-28 12:55:45 +0200

    Improvement of code snippets

--- ChrisBlog/src/main/webapp/scripts/script.js
+++ ChrisBlog/src/main/webapp/scripts/script.js
@@ -6,3 +6,12 @@ function send(action){
 	document.form.action = action;
 	document.form.submit();	
 }
+
+/**
+ * Function changes specified object style 'overflow' to 'auto' and hidden borders.
+ *  
+ * @param obj with changed style
+ */
+function overflowAuto(obj){
+	$(obj).attr("style", "overflow:auto !important; border-right-style:hidden; border-bottom-style: hidden;");
+}
