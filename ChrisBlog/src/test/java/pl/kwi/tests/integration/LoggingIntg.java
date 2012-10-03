64fd1284344ac44d6773493dd05495459fbcea51
Krzysztof Wisniewski <wisniewskik@op.pl>
2012-10-03 14:01:33 +0200

    Implementing loging mechanism: change logging integration test

--- ChrisBlog/src/test/java/pl/kwi/tests/integration/LoggingIntg.java
+++ ChrisBlog/src/test/java/pl/kwi/tests/integration/LoggingIntg.java
@@ -4,16 +4,12 @@ import junit.framework.Assert;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.transaction.annotation.Transactional;
 
 public class LoggingIntg{
 	
@@ -36,9 +32,6 @@ public class LoggingIntg{
 	@Test
 	public void testLogging(){
 		
-		// TODO KWi
-//		executeDataFile("dbunit/loggingIntg.xml");
-		
 		driver = new HtmlUnitDriver();
 		wait = new WebDriverWait(driver, 20);
 
@@ -54,25 +47,10 @@ public class LoggingIntg{
         text = driver.findElement(By.id("hello_world_servlets_titleList")).getText();
         Assert.assertEquals("Hello World Servlets", text);
         // actions
-        driver.findElement(By.id("login")).sendKeys("admin");
-        driver.findElement(By.id("password")).sendKeys("admin");
+        driver.findElement(By.id("login")).sendKeys("tmp");
+        driver.findElement(By.id("password")).sendKeys("tmp");
         driver.findElement(By.id("signIn")).click();
-        
-        
-        // Secured article list
-        wait.until(ExpectedConditions.textToBePresentInElement(By.id("headerTitle"), "Chris`s Blog"));
-        // conditions
-        header = driver.findElement(By.id("headerTitle")).getText();
-        Assert.assertEquals("Chris`s Blog", header);        
-        title = driver.getTitle();
-        Assert.assertEquals("Chris`s Blog - Secured List of Articles", title);
-        text = driver.findElement(By.id("userName")).getText();
-        Assert.assertEquals("admin", text);
-        text = driver.findElement(By.id("userRole")).getText();
-        Assert.assertEquals("[ROLE_USER]", text);
-        // actions
-        driver.findElement(By.id("signOut")).click();
-        
+       
         
         // Article list
         wait.until(ExpectedConditions.textToBePresentInElement(By.id("headerTitle"), "Chris`s Blog"));
@@ -83,6 +61,8 @@ public class LoggingIntg{
         Assert.assertEquals("Chris`s Blog - List of Articles", title);
         text = driver.findElement(By.id("hello_world_servlets_titleList")).getText();
         Assert.assertEquals("Hello World Servlets", text);
+        text = driver.findElement(By.className("textMsg")).getText();
+        Assert.assertEquals("Incorrect username or password.", text);
         
 	}
 	
