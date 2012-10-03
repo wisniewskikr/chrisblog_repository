ee931ecbfde031ea399f4f89e6043fc546fdbb56
Krzysztof Wisniewski <wisniewskik@op.pl>
2012-10-03 12:17:11 +0200

    Implementing loging mechanism: add comments to AbstractDao

--- ChrisBlog/src/main/java/pl/kwi/db/abstr/AbstractDao.java
+++ ChrisBlog/src/main/java/pl/kwi/db/abstr/AbstractDao.java
@@ -6,13 +6,21 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+/**
+ * Abstract class for daos. It implements methods of CRUD.
+ * 
+ * @author Krzysztof Wisniewski
+ *
+ * @param <T> object implements Serializable
+ */
 public class AbstractDao < T extends Serializable >{
+
 	
+		@PersistenceContext
+		private EntityManager entityManager;
 		   
 	   private Class< T > clazz;
 	   
-	   @PersistenceContext
-	   private EntityManager entityManager;
 	   
 	   public void setClazz( final Class< T > clazzToSet ){
 	      this.clazz = clazzToSet;
@@ -21,6 +29,7 @@ public class AbstractDao < T extends Serializable >{
 	   public T findOne( final Long id ){
 	      return entityManager.find( clazz, id );
 	   }
+	   
 	   public List< T > findAll(){
 	      return entityManager.createQuery( "from " + clazz.getName() )
 	       .getResultList();
