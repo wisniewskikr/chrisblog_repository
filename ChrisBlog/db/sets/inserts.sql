1d6165c1730f52de39f1cb8995d8838421e2d927
Krzysztof Wisniewski <wisniewskik@op.pl>
2012-10-07 11:32:01 +0200

    #8 Implementing entities in db: add first article to db

--- ChrisBlog/db/sets/inserts.sql
+++ ChrisBlog/db/sets/inserts.sql
@@ -7,3 +7,8 @@ INSERT INTO user_roles (ID, USER_ID,AUTHORITY)
 VALUES (1, 1, 'ROLE_USER');
 
 GO
+
+INSERT INTO articles (ID, UNIQUE_NAME, TITLE, DESCRIPTION, CONTENT, PAGES_COUNT, CREATION_DATE, AUTHOR, DEMO_NAME, EXAMPLE_FILE_NAME, SOURCE_FILE_NAME)
+VALUES (1, 'hello_world_servlets', 'Hello World Servlets', 'helloWorldServletsJsp', 'helloWorldServletsJsp', 4, '2012-01-04 12:00:00', 'Chris', 'HelloWorldServlets', 'HelloWorldServlets.war', 'HelloWorldServlets.zip');
+
+GO
